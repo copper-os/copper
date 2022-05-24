fab2214b23dfaa705020950e8990920d599a5658
Kitae Kim <develoot@gmail.com>
2022-05-25 02:46:51 +0900

    Remove invalid `init_console()` code

--- kernel/lib/console/console.c
+++ kernel/lib/console/console.c
@@ -1,18 +1,4 @@
-#include <console/console.h>
-#include <efi/efi.h>
-#include <efi/efilib.h>
-
 int console_init(void)
 {
-    EFI_STATUS status = 0;
-
-    EFI_GRAPHICS_OUTPUT_PROTOCOL* graphics_output = NULL;
-    status = uefi_call_wrapper(BS->LocateProtocol, 3, &gEfiGraphicsOutputProtocolGuid, NULL,
-                               (void**)&graphics_output);
-    if (EFI_ERROR(status)) {
-        Print(L"Failed to locate GraphicsOutputProtocol. %r\n", status);
-        return 1;
-    }
-
     return 0;
 }
